ef76d03a21354400f4703c1a9639814b65d17526
Sasha Gerrand <github-homebrew-confluent-platform@sgerrand.com>
2018-04-18 09:08:37 +0100

    :up: Bump Confluent Platform version to 4.1.0

--- Formula/confluent-oss.rb
+++ Formula/confluent-oss.rb
@@ -1,9 +1,9 @@
 class ConfluentOss < Formula
   desc "Developer-optimized distribution of Apache Kafka"
   homepage "https://www.confluent.io/product/confluent-open-source/"
-  url "https://packages.confluent.io/archive/4.0/confluent-oss-4.0.1-2.11.tar.gz"
-  version "4.0.1"
-  sha256 "335d1440a7cd18341a62c5e2584e8e0f7d52c3070940a60bde3166ad8bb1100e"
+  url "https://packages.confluent.io/archive/4.1/confluent-oss-4.1.0-2.11.tar.gz"
+  version "4.1.0"
+  sha256 "32c3aff688d6f24e1ea573efdc4e16a3a5f466f1641b770ae4ba0350e7be583a"
 
   depends_on :java => "1.8"
   conflicts_with "kafka", :because => "kafka also ships with identically named Kafka related executables"
